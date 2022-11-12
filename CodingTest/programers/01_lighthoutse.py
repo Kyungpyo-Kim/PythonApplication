34c2b79f334ccd2cc840a2dda2388ba19c1d0198
Kyungpyo Kim <kyungpyokim@Kyungpyos-MacBook-Air.local>
2022-11-12 10:04:55 +0900

    start programers coding test

---
+++ CodingTest/programers/01_lighthoutse.py
@@ -0,0 +1,41 @@
+import sys
+
+sys.setrecursionlimit(1000000)
+
+def dfs(node, tree, visited):
+    visited[node] = True
+    children = [child for child in tree[node] if not visited[child]]
+    pick, not_pick = 1, 0
+    
+    if not children:
+        return pick, not_pick
+
+    for child in children:
+        child_pick, child_not_pick = dfs(child, tree, visited)
+        pick += min(child_pick, child_not_pick)
+        not_pick += child_pick
+    return pick, not_pick
+
+
+def solution(n, lighthouse):
+
+    # make tree from lighthouse
+    tree = [[] for _ in range(n+1)]
+    for a, b in lighthouse:
+        tree[a].append(b)
+        tree[b].append(a)
+
+    visited = [False for _ in range(n+1)]
+    root = 1
+
+    return min(dfs(root, tree, visited))
+
+if __name__ == "__main__":
+    n = 8
+    lighthouse = [[1, 2], [1, 3], [1, 4], [1, 5], [5, 6], [5, 7], [5, 8]]
+    print(solution(n, lighthouse))
+
+    n = 10
+    lighthouse = [[4, 1], [5, 1], [5, 6], [7, 6], [1, 2], [1, 3], [6, 8], [2, 9], [9, 10]]
+    print(solution(n, lighthouse))
+
---
+++ CodingTest/programers/02_roads.py
@@ -0,0 +1,38 @@
+from collections import defaultdict,deque
+
+def solution(n, roads, sources, destination):
+    tree = defaultdict(list)
+
+    for a, b in roads:
+        tree[a].append(b)
+        tree[b].append(a)
+
+    visited = set()
+    distance_map = [-1 for _ in range(n+1)]
+
+    queue = deque([destination])
+    visited.add(destination)
+    distance_map[destination] = 0
+    while queue:
+        node = queue.popleft()
+        children = [child for child in tree[node] if child not in visited]
+        for child in children:
+            visited.add(child)
+            distance_map[child] = distance_map[node] + 1
+            queue.append(child)
+
+    return [distance_map[source] for source in sources]
+
+
+if __name__ == "__main__":
+    n = 3
+    roads = [[1, 2], [2, 3]]	
+    sources = [2, 3]
+    destination = 1
+    print(solution(n, roads, sources, destination))
+
+    n = 5
+    roads = [[1, 2], [1, 4], [2, 4], [2, 5], [4, 5]]	
+    sources = [1, 3, 5]	
+    destination = 5	
+    print(solution(n, roads, sources, destination))
